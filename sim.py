d82565026173b3ad5b1feb02148d98bae63d03b7
MBoudreau-IDM <mariah.boudreau@gatesfoundation.org>
2022-07-07 15:18:43 -0700

    changing the import statements

--- sim.py
+++ sim.py
@@ -6,17 +6,17 @@ Define core Sim classes
 import numpy as np
 import pandas as pd
 import sciris as sc
-from . import base as cellBase
+import base as cellBase
 from . import misc as cellMisc
-from . import default as cellDef
-from . import utils as cellUtil
-from . import population as cellPop
-from . import parameters as cellPar
-from . import analysis as cellA
-from . import plotting as cellPlt
-from .settings import options as cellOp
-from . import cell_mass as cellMass
-from . import event_driven as cellEvent
+import default as cellDef
+import utils as cellUtil
+import population as cellPop
+import parameters as cellPar
+import analysis as cellA
+import plotting as cellPlt
+from . settings import options as cellOp
+import cell_mass as cellMass
+
 
 
 
