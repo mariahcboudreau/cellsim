4b42e45457b411e5837302b4ce8fbf3d22a4c720
MBoudreau-IDM <mariah.boudreau@gatesfoundation.org>
2022-07-15 14:33:52 -0700

    Debugging

--- test.py
+++ test.py
@@ -4,6 +4,21 @@ Tests the current code
 
 '''
 
-import sim as cellsim
+import numpy as np
+import pandas as pd
+import sciris as sc
+import base as cellBase
+import sim as cellSim
+from . import misc as cellMisc
+import default as cellDef
+import utils as cellUtil
+import population as cellPop
+import parameters as cellPar
+import analysis as cellA
+import plotting as cellPlt
+from . settings import options as cellOp
+import cell_mass as cellMass
 
-cellsim.Sim()
+
+sim = cellSim.Sim()
+
